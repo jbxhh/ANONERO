bf5dd4c91536cbcdb75c6b668d0653b01fce662e
jbxhh <fgf166074@gmail.com>
2026-09-06 08:21:36 +0800

    Update Send.kt

--- app/src/main/java/io/anonero/ui/home/Send.kt
+++ app/src/main/java/io/anonero/ui/home/Send.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
