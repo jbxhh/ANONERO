9d863d2160a89c1b554269c8c09bbc290a95b163
jbxhh <fgf166074@gmail.com>
2026-09-06 21:23:45 +0800

    Update Subaddress.kt

--- app/src/main/java/io/anonero/model/Subaddress.kt
+++ app/src/main/java/io/anonero/model/Subaddress.kt
@@ -37,8 +37,9 @@ class Subaddress(
 
             val displayLabel: String
         get() = if (label.isEmpty() || DEFAULT_LABEL_FORMATTER.matcher(label)
-                .matches() || label.startsWith("Subaddress1")
-        ) "" else "$label $addressIndex"
+                .matches() || label.startsWith("Subaddress")
+              ) "" else "$"
+
 
     companion object {
         val DEFAULT_LABEL_FORMATTER: Pattern =
