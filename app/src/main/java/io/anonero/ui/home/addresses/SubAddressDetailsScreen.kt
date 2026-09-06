766d9647b23d810fe373f3c255b7d27d3391feac
jbxhh <fgf166074@gmail.com>
2026-09-06 12:48:18 +0800

    Update SubAddressDetailsScreen.kt

--- app/src/main/java/io/anonero/ui/home/addresses/SubAddressDetailsScreen.kt
+++ app/src/main/java/io/anonero/ui/home/addresses/SubAddressDetailsScreen.kt
@@ -80,7 +80,12 @@ fun SubAddressDetailScreen(
         SubAddressDetail(subAddress)
     }
     val transactions by subAddressDetailVm.transactions.observeAsState(listOf())
-    var addressLabel by remember { mutableStateOf(subAddress.displayLabel) }
+        var addressLabel by remember {
+        mutableStateOf(
+            if (subAddress.label.isEmpty() || subAddress.label.startsWith("Subaddress #"))
+                "" else subAddress.displayLabel
+        )
+    }
     BackHandler {
         onBackPress()
     }
