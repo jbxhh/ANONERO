2b0ed816cfd3976d4d68f4b0ef3c42c821be5605
jbxhh <fgf166074@gmail.com>
2026-09-07 09:22:20 +0800

    Fix typo in 'tStyle' to 'Style' in Send.kt

--- app/src/main/java/io/anonero/ui/home/Send.kt
+++ app/src/main/java/io/anonero/ui/home/Send.kt
@@ -571,7 +571,7 @@ fun SendScreen(
                                 }
                             },
                         textAlign = TextAlign.Center,
-                        tStyle = MaterialTheme.typography.bodyLarge.copy(letterSpacing = 0.sp, fontFamily = FontFamily.Default), 
+                        Style = MaterialTheme.typography.bodyLarge.copy(letterSpacing = 0.sp, fontFamily = FontFamily.Default), 
                     )
                         IconButton(
                         modifier = Modifier.align(Alignment.CenterHorizontally),
