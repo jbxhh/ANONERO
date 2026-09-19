b5e8fc0c2cb34d6298de9fb0dc151d0cbb7ab5f0
jbxhh <fgf166074@gmail.com>
2026-09-20 02:34:43 +0800

    Update Theme.kt

--- app/src/main/java/io/anonero/ui/theme/Theme.kt
+++ app/src/main/java/io/anonero/ui/theme/Theme.kt
@@ -1,3 +1,44 @@
+package io.anonero.ui.theme
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import io.anonero.R
+import io.anonero.ui.theme.Black
+import io.anonero.ui.theme.DarkGray
+import io.anonero.ui.theme.DarkOrange
+import io.anonero.ui.theme.Orange
+import io.anonero.ui.theme.White
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Orange,
+    onPrimary = Black,
+    primaryContainer = DarkOrange,
+    onPrimaryContainer = White,
+    secondary = DarkGray,
+    onSecondary = White,
+    background = Black,
+    onBackground = White,
+    surface = Black,
+    onSurface = White
+)
+
+@Composable
+fun AnonNeroTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = DarkColorScheme,
+        content = content,
         typography = Typography.copy(
             titleLarge = Typography.titleLarge.copy(fontFamily = FontFamily.SansSerif),
             titleSmall = Typography.titleSmall.copy(fontFamily = FontFamily.SansSerif),
@@ -15,3 +56,23 @@
             labelSmall = Typography.labelSmall.copy(fontFamily = FontFamily.SansSerif),
             labelMedium = Typography.labelMedium.copy(fontFamily = FontFamily.SansSerif),
         )
+    )
+}
+
+@Composable
+fun AnonOutlineButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+    child: @Composable () -> Unit
+) {
+    OutlinedButton(
+        enabled = enabled,
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        child()
+    }
+}
