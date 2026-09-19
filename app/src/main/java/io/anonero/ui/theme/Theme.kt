6c942533ab8db22aeb5d4466d0fb043ce9182c58
jbxhh <fgf166074@gmail.com>
2026-09-20 02:40:08 +0800

    Update Theme.kt

--- app/src/main/java/io/anonero/ui/theme/Theme.kt
+++ app/src/main/java/io/anonero/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package io.anonero.ui.theme
 
+import io.anonero.ui.theme.Typography
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
