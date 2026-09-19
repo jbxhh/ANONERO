3df74383d8412564ba07d38a6ebd9d916e9d2375
jbxhh <fgf166074@gmail.com>
2026-09-20 02:30:32 +0800

    Update Theme.kt

--- app/src/main/java/io/anonero/ui/theme/Theme.kt
+++ app/src/main/java/io/anonero/ui/theme/Theme.kt
@@ -1,87 +1,17 @@
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import io.anonero.R
-import io.anonero.ui.theme.Black
-import io.anonero.ui.theme.DarkGray
-import io.anonero.ui.theme.DarkOrange
-import io.anonero.ui.theme.Orange
-import io.anonero.ui.theme.Typography
-import io.anonero.ui.theme.White
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Orange,
-    onPrimary = Black,
-    primaryContainer = DarkOrange,
-    onPrimaryContainer = White,
-    secondary = DarkGray,
-    onSecondary = White,
-    background = Black,
-    onBackground = White,
-    surface = Black,
-    onSurface = White
-)
-
-val robotoMonoFamily = FontFamily(
-    Font(R.font.robotomono_light, FontWeight.Light),
-    Font(R.font.robotomono_regular, FontWeight.Normal),
-    Font(R.font.robotomono_medium, FontWeight.Medium),
-    Font(R.font.robotomono_bold, FontWeight.Bold),
-    Font(R.font.robotomono_semi_bold, FontWeight.SemiBold),
-)
-
-
-@Composable
-fun AnonNeroTheme(
-    content: @Composable () -> Unit
-) {
-    MaterialTheme(
-        colorScheme = DarkColorScheme,
-        content = content,
         typography = Typography.copy(
-            titleLarge = Typography.titleLarge.copy(fontFamily = robotoMonoFamily),
-            titleSmall = Typography.titleSmall.copy(fontFamily = robotoMonoFamily),
-            titleMedium = Typography.titleMedium.copy(fontFamily = robotoMonoFamily),
-            headlineMedium = Typography.headlineMedium.copy(fontFamily = robotoMonoFamily),
-            headlineLarge = Typography.headlineLarge.copy(fontFamily = robotoMonoFamily),
-            headlineSmall = Typography.headlineSmall.copy(fontFamily = robotoMonoFamily),
-            bodyLarge = Typography.bodyLarge.copy(fontFamily = robotoMonoFamily),
-            bodySmall = Typography.bodySmall.copy(fontFamily = robotoMonoFamily),
-            bodyMedium = Typography.bodyMedium.copy(fontFamily = robotoMonoFamily),
-            displayLarge = Typography.displayLarge.copy(fontFamily = robotoMonoFamily),
-            displaySmall = Typography.displaySmall.copy(fontFamily = robotoMonoFamily),
-            displayMedium = Typography.displayMedium.copy(fontFamily = robotoMonoFamily),
-            labelLarge = Typography.labelLarge.copy(fontFamily = robotoMonoFamily),
-            labelSmall = Typography.labelSmall.copy(fontFamily = robotoMonoFamily),
-            labelMedium = Typography.labelMedium.copy(fontFamily = robotoMonoFamily),
+            titleLarge = Typography.titleLarge.copy(fontFamily = FontFamily.SansSerif),
+            titleSmall = Typography.titleSmall.copy(fontFamily = FontFamily.SansSerif),
+            titleMedium = Typography.titleMedium.copy(fontFamily = FontFamily.SansSerif),
+            headlineMedium = Typography.headlineMedium.copy(fontFamily = FontFamily.SansSerif),
+            headlineLarge = Typography.headlineLarge.copy(fontFamily = FontFamily.SansSerif),
+            headlineSmall = Typography.headlineSmall.copy(fontFamily = FontFamily.SansSerif),
+            bodyLarge = Typography.bodyLarge.copy(fontFamily = FontFamily.SansSerif),
+            bodySmall = Typography.bodySmall.copy(fontFamily = FontFamily.SansSerif),
+            bodyMedium = Typography.bodyMedium.copy(fontFamily = FontFamily.SansSerif),
+            displayLarge = Typography.displayLarge.copy(fontFamily = FontFamily.SansSerif),
+            displaySmall = Typography.displaySmall.copy(fontFamily = FontFamily.SansSerif),
+            displayMedium = Typography.displayMedium.copy(fontFamily = FontFamily.SansSerif),
+            labelLarge = Typography.labelLarge.copy(fontFamily = FontFamily.SansSerif),
+            labelSmall = Typography.labelSmall.copy(fontFamily = FontFamily.SansSerif),
+            labelMedium = Typography.labelMedium.copy(fontFamily = FontFamily.SansSerif),
         )
-    )
-}
-
-@Composable
-fun AnonOutlineButton(
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    onClick: () -> Unit,
-    child: @Composable () -> Unit
-) {
-    OutlinedButton(
-        enabled = enabled,
-        onClick = onClick,
-        modifier = modifier
-            .fillMaxWidth(),
-        shape = MaterialTheme.shapes.medium,
-        contentPadding = PaddingValues(16.dp)
-    ) {
-        child()
-    }
-}
